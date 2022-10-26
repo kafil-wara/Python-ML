5eb448747292d45f11bd7d168ce1fefa0dd23053
kafil <kafilwr@gmail.com>
2022-10-26 23:08:03 +0600

    final commit

--- MidTerm/Course_Management_Console_App.py
+++ MidTerm/Course_Management_Console_App.py
@@ -1,3 +1,6 @@
+import pickle
+from fileinput import filename
+
 course_list = []
 
 class Course:
@@ -7,52 +10,82 @@ class Course:
         self.credit = credit
         self.prerequisites = prerequisites
 
-    # def print_Details(self):
-    #     for obj in course_List:   
-    #         print(self.name)
-    #         print(self.code)
-    #         print(self.credit)
-    #         print(self.prerequisites)
-
-def add_Course():
-    code = input("Course Code: ")
+def add_Course(*arg):
+    if arg:
+        code = ''
+        for a in arg:
+            code += a
+    else:
+        code = input("Course Code: ")
     name = input("Name: ")
     credit = input("No. of Credits: ")
     prerequisites = [item for item in input("Prerequisites: ").split()]
-    # if len(prerequisites) == 0:
-        #     add_Course(code, name, credit, prerequisites)
-        # elif prereq_check(prerequisites):
-        #     add_Course(code, name, credit, prerequisites)
-        # else:
-        #     print("?????????????????????????")
-    course_list.append(Course(code, name, credit, prerequisites))
-    print("Course added!")
+    if len(prerequisites) != 0:
+        if prereq_check(prerequisites):
+            course_list.append(Course(code, name, credit, prerequisites))
+            print("Course added!!!")
+        else:
+            for prereq in prerequisites:
+                add_Course(prereq)
+            
+    else:
+        course_list.append(Course(code, name, credit, prerequisites))
+        print("Course added!")
     
-# def prereq_check(args):
-#     for prereqs in args:
-#         for courses in course_list:
-#             if prereqs not in courses.code:
-#                 return False
-#             else:
-#                 return True
+    
+    
+def prereq_check(args):
+    code_set = set()
+    arg_set = set()
+    for course in course_list:
+        code_set.add(course.code)
+    for a in args:
+        arg_set.add(a)
+    if arg_set.issubset(code_set):
+        return True
+    else:
+        for arg in arg_set:      
+            a = set()
+            a.add(arg)
+            if a.issubset(code_set):
+                pass
+            else:
+                print(arg + " is not in the course list")
+                inp = input("Do you want to add this course? Press 1 to add, any other key to pass\n")
+                if inp == '1':
+                    add_Course(arg)
+                else:
+                    return False
 
 def search(code):
-    #USE SINGLE COURSE SHOW FN TO SHOW DEETS
     for obj in course_list:
         if obj.code == code:
             return obj
-        else:
-            print("Course Not Found!")
+    print("Course Not Found!")
+    inp = input("Would you like to add this course? Press 1 to add, any other key to pass\n")
+    if inp == '1':
+        add_Course(code)
+    else:
+        pass
 
 def all_course_display():
     for obj in course_list:
-            print(obj.code, obj.name, obj.credit, obj.prerequisites)
+        print("Course Code: " + obj.code)
+        print("Course Name: " + obj.name)
+        print("Course Credit: " + obj.credit)
+        print("Course Prerequisites: ", end='')
+        print(obj.prerequisites)
+        print()
 
 def single_course_display(code):
     course = search(code)
     if course:
-        print(course.code, course.name, course.credit, course.prerequisites)
-    ### NEED TO ADD PROMPT TO ADD
+        print("Course Code: " + course.code)
+        print("Course Name: " + course.name)
+        print("Course Credit: " + course.credit)
+        print("Course Prerequisites: ", end='')
+        print(course.prerequisites)
+        print()
 
 def delete_course():
     global course_list
@@ -65,11 +98,64 @@ def delete_course():
         print("Deletion Successful")
 
 def update_course():
-    code = input("Enter Course Code to update: ")
-    course = search(code)
+    update_code = input("Enter Course Code to update: ")
+    course = search(update_code)
+    code = course.code
+    name = course.name
+    credit = course.credit
+    prerequisites = course.prerequisites
     single_course_display(code)
-    print("Please update or leave blank to keep as is")
+    print('''
+            Choose info to update:
+            1. Name
+            2. Code
+            3. Credit
+            4. Prerequisites
+            ''')
+    inp = input()
+    if inp == '1':
+        new_name = input("New Name: ")
+        course.name = new_name
+        print("Name Updated!")
+    elif inp == '2':
+        new_code = input("New Code: ")
+        course.code = new_code
+        print("Code Updated!")
+    elif inp == '3':
+        new_credit = input("New Credit: ")
+        course.credit = new_credit
+        print("Credit Updated!")
+    elif inp == '4':
+        new_prerequisites = [item for item in input("Prerequisites: ").split()]
+        if prereq_check(new_prerequisites):
+            course.prerequisites = new_prerequisites
+            print("Prerequisites Updated!")
+        else:
+            print("Error updating prerequisites!")
+    
 
+
+
+def perma_store():
+    try:
+        with open('storage.pkl', 'wb') as outp:
+            pickle.dump(course_list, outp)
+            print("Write to file successful")
+            outp.close()
+    except IOError:
+        print("IO Error!")
+
+def read_storage():
+    objects = []
+    with (open("storage.pkl", "rb")) as openfile:
+        c_list = pickle.load(openfile)
+    for c in c_list:
+        print("Course Code: " + c.code)
+        print("Course Name: " + c.name)
+        print("Course Credit: " + c.credit)
+        print("Course Prerequisites: ", end='')
+        print(c.prerequisites)
+        print()
 
 
     
@@ -77,41 +163,39 @@ def update_course():
 while(True):
     print(
     '''
-    1. Add
-    2. print deets
-    3. single course display
-    4. Delete
-    5. Update
+    1. Add New Course
+    2. Display All Courses
+    3. Display Individual Course
+    4. Search
+    5. Delete Course
+    6. Update Course
+    7. Store Course List
+    8. Read Stored Course List
+    Enter 'quit' to exit
     '''
     )
 
-    inp = input()
+    inp = input("Choose Option: ")
     if inp == '1':
         add_Course()
-        
-        
     elif inp == '2':
         all_course_display()
     elif inp == '3':
         code = input("Enter Course Code: ")
         single_course_display(code)
     elif inp == '4':
-        delete_course()
+        code = input("Enter Course Code to Search: ")
+        single_course_display(code)
     elif inp == '5':
+        delete_course()
+    elif inp == '6':
         update_course()
+    elif inp == '7':
+        perma_store()
+    elif inp == '8':
+        read_storage()
     elif inp == 'quit':
         exit()
     else:
-        print("XXX")
-
-
-
-
-
-
-
-
-def perma_store():
-    pass
-
+        print("Wrong Input")
 
